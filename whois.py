da519d67cbf1dc5bca677ef6cd823ef33ef55bcc
Haris Shariff <harisms.2022@scis.smu.edu.sg>
2023-10-06 19:31:06 +0800

    whois Script added with IP address Country and city

---
+++ whois.py
@@ -0,0 +1,32 @@
+import subprocess
+from datetime import datetime
+import re
+
+inputFile = input('Enter filename here: ')
+filename = str(datetime.now().strftime('%d-%m-%Y %H%M HRS')) + '_whois_output.txt'
+print('whois analysis loading...')
+# Open the text file containing IP addresses
+with open(inputFile, 'r') as textfile:
+    # Create or open the output file for country information
+    with open(filename, 'w') as outputfile:
+        for line in textfile:
+            # Remove leading and trailing whitespace from each line
+            ip = line.strip()
+
+            # Run the whois command and capture its output
+            whois_output = subprocess.check_output(['whois', ip], universal_newlines=True)
+
+            # Search for the country information in the whois output
+            country = ''
+            city = ''
+            for whois_line in whois_output.split('\n'):
+                if re.search(r'(?i)Country', whois_line):
+                    country = whois_line.split(':')[-1].strip()
+                elif re.search(r'(?i)City', whois_line):
+                    city = whois_line.split(':')[-1].strip()
+
+            # Write the IP address and country information to the output file
+            outputfile.write(f"IP Address: {ip}, Country: {country}, City: {city}\n")   
+
+print("whois extraction completed.")
+
