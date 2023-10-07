f549abc71a00e44fd1ec08a103ac1afafe33c94c
Haris Shariff <harisms.2022@scis.smu.edu.sg>
2023-10-08 00:19:38 +0800

    Added README and logs for traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -1,44 +1,58 @@
 import subprocess
-from datetime import datetime
+import re
+import csv
+import datetime
 
-print('Starting traceroute...')
-# Run traceroute to www.google.com with a timeout for each hop and store the output in a file
-traceroute_process = subprocess.Popen(['traceroute', '-w', '1', 'www.google.com'], stdout=subprocess.PIPE)
+vpn = "Singapore"
+with open("target.csv", "r") as file:
+    websites = [line.strip() for line in file]
+
+results = []
+
+for website in websites:
+    try:
+        print(f'Tracerouting {website} - Do not end the process')
+        cmd_output = subprocess.check_output(['traceroute', '-w', '5', '-m', '10', website], universal_newlines=True, stderr=subprocess.STDOUT)
+
+        current_hop = None
+        sub_hop = 0
+        for line in cmd_output.split("\n"):
+            print("=====")
+            print(line)
+
+            if line.startswith("traceroute:") or line.startswith("traceroute to") or line.strip() == "* * *":
+                continue
+            match = re.match(r"\s*(\d+)?\s+.*\s?\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)\s+([\d\.]+)\s+ms(?:\s+([\d\.]+)?\s*ms?)?(?:\s+([\d\.]+)?\s*ms?)?", line)
+
+            if match:
+                print("Captured Groups:", match.groups())
+
+                if match.group(1):
+                    print("match1")
+                    hop = match.group(1)
+                    current_hop = hop
+                    sub_hop = 0
+                else:
+                    print("match2")
+                    sub_hop += 1
+                    hop = f"{current_hop}.{sub_hop}"
+
+                ip = match.group(2)
+                speed1 = match.group(3)
+                speed2 = match.group(4) if match.group(4) else ""
+                speed3 = match.group(5) if match.group(5) else ""
+
+                print([website, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), hop, vpn, ip, speed1, speed2, speed3])
+                results.append([website, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), hop, vpn, ip, speed1, speed2, speed3])
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing traceroute for {website}: {str(e)}")
+
+print('Extracting traceroute')
 tracerouteFileName = str(datetime.now().strftime('%d-%m-%Y %H%M HRS')) + '_traceroute.output.txt'
-ipFileName = str(datetime.now().strftime('%d-%m-%Y %H%M HRS')) + '_ip_address.output.txt'
+with open(tracerouteFileName, "w", newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['website','datetime', 'hop', 'vpn', 'ip', 'speed1', 'speed2', 'speed3'])
+    writer.writerows(results)
 
-print('Writing to ', tracerouteFileName)
-with open(tracerouteFileName, 'wb') as output_file:
-    lineCount = 0
-    while True:
-
-        line = traceroute_process.stdout.readline()
-        lineCount += 1
-        if not line:
-            break
-
-        output_file.write(line)
-        output_file.flush()  # Flush the output to the file immediately
-    print('Traceroute Completed')
-    print('{} number of lines processed.'.format(lineCount))
-
-print('Writing to ', ipFileName)
-# Extract IP addresses from the traceroute output and save them to a file
-with open(tracerouteFileName, 'r') as input_file, open(ipFileName, 'w') as output_file:
-    count = 0
-    for line in input_file:
-        columnNo = 0
-        # Assuming that IP addresses are in the second column, separated by spaces
-        columns = line.strip().split()
-        for columnNo, column in enumerate(columns):
-            if column[0] == '(':
-                ip_address = columns[columnNo]
-                break
-            columnNo += 1
-        if (ip_address[0] != '*'):
-            if (ip_address[0] == '('):
-                ip_address = ip_address.replace('(', '').replace(')', '')
-            output_file.write(ip_address + '\n')
-            count += 1
-    print('IP address extraction completed.')
-    print('{} number of IP addresses'.format(count))
+print(f'{tracerouteFileName} extraction completed.')
